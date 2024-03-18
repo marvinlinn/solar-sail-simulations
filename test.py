74b65eb802484bb5240b5d26d2721964bfd7e5f1
Andrew Ji <jiandrew10@gmail.com>
2024-03-18 11:05:17 -0700

    basic generator, will update when functional

--- test.py
+++ test.py
@@ -3,10 +3,18 @@ import functions.system as system
 import functions.body as body
 import functions.utils as utils
 import numpy as np
+from scipy.integrate import solve_ivp
 
 solar_system = system.SolarSystem("solar")
-solar_system.animateBodies()
-utils.animatebodies(solar_system.bodies, 15)
+#solar_system.animateBodies()
+
+print(solar_system.bodies[3].locations.T[0])
+
+sail = body.SolarSail("sail1", 0,0,0,0)
+sail.locations = solve_ivp(utils.simpleSailGenerator, [0, 3.2e7], [1.496e11, 0, 0, 30], rtol=1e-8, args=[-0.6])[:2, :]
+
+print(sail.locations)
+#utils.animatebodies(np.append(solar_system.bodies), 15)
 
 vect = np.array([0,0,1])
 print(vect)
