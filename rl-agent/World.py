561edf73b161f9d1f800275689e1a5978ecec528
Luke Harris <harris_luke@icloud.com>
2024-04-21 20:47:48 -0700

    reward

--- rl-agent/World.py
+++ rl-agent/World.py
@@ -40,8 +40,11 @@ class SimpleTestWorld(World):
         accel = 0.1
         x, y, vx, vy = 0, 1, 3, 4
 
-        accel_x = accel * math.cos(a)
-        accel_y = accel * math.sin(a)
+        x0 = self.s[x]
+        y0 = self.s[y]
+
+        accel_x = accel * a # math.cos(a)
+        accel_y = 0 # accel * math.sin(a)
 
         self.s[x] += 1/2 * accel_x * dt**2 + self.s[vx] * dt
         self.s[y] += 1/2 * accel_y * dt**2 + self.s[vy] * dt
@@ -51,6 +54,14 @@ class SimpleTestWorld(World):
 
         tx, ty = 6, 7
 
-        reward = 1/max(1/200, (self.s[x] - self.s[tx])**2 + (self.s[y] - self.s[ty])**2)
+        d0 = math.sqrt((x0-self.s[tx])**2 + (y0-self.s[ty])**2)
+        df = math.sqrt((self.s[x]-self.s[tx])**2 + (self.s[y]-self.s[ty])**2)
+
+        v_error = self.s[vx]**2 + self.s[vy]**2
+
+        pen_neg = lambda x: min(10*df*x, x)
+
+        reward = pen_neg(d0-df) + \
+                1/max(1/1000, df**2) + max(7-df,5-df/5, 0)
         
         return reward, self.s
