00342d6d40f5e777c7374b9cdafcf452d06ba008
Andrew Ji <jiandrew10@gmail.com>
2024-05-20 20:08:16 -0700

    2d packaged sim for training purposes

--- functions/utils.py
+++ functions/utils.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import math
 import functions.body as body
+import functions.system as system
 import scipy.integrate as integ
+import stateCollection.spiceInterface as spice
 #important constants:
 G = 6.6743E-11
 
@@ -262,6 +264,63 @@ def animatebodies(bodies, tstep=1):
     ani = animation.FuncAnimation(fig, update, numframes, fargs=(bodies, lines), interval=100/numframes, blit=False)
     ani.save('solarsystem.gif')
     plt.show()
+
+
+def packaged2DSim(simTime, sailorientations, numsailchanges):
+    timeSeconds = simTime.lengthSeconds
+
+    #planet generation
+    sysname = str(simTime.length) + " day sys"
+    sys = system.SolarSystem(sysname, simTime)
+    sysbds = sys.bodies
+    numSteps = len(sysbds[0].locations[0])
+
+
+    #trajectories generation
+    pitches = np.zeros(numsailchanges+1)
+    yaws = permutationGenerator(sailorientations, numsailchanges+1)
+    timeInt = np.zeros(numsailchanges+1)
+    for n in range(numsailchanges+1):
+        timeInt[n] = n * timeSeconds
+
+    #sail generation
+    #init conditions -> earth position, velocity must also be vectorized correctly
+    initPos = sysbds[3].locations.T[0]
+    initVelVec = (sysbds[3].locations.T[1]-sysbds[3].locations.T[0])/np.linalg.norm(sysbds[3].locations.T[1]-sysbds[3].locations.T[0]) #velocity vector via linearization between point 0 and 1
+    initVel = initVelVec * 30
+    sailset = np.array([])
+
+
+    for n in range(len(yaws)):
+        newSail = sailGenerator(("sail"+ str(n)), initPos, initVel,
+                                  np.array([timeInt, yaws[n], pitches]), [0, timeSeconds], numSteps)
+        sailset = np.append(sailset, newSail)
+
+    simset = np.append(sailset, sysbds)
+    
+    return simset
+
+#generates an array filled with all possible state combinations for a given length
+#recursive -> head is passing the already determined pieces down the recursion
+def permutationGenerator(states, length, head=np.array([])):
+    retArray = np.array([])
+
+    if length == 1:
+        for state in states:
+            if len(retArray) == 0:
+                retArray = np.array([np.append(head, state)])
+            else:
+                retArray = np.append(retArray, [np.append(head, state)], axis=0)
+        return retArray
+    
+    for state in states:
+        newhead = np.append(head, np.array([state]))
+        if len(retArray) == 0:
+            retArray = permutationGenerator(states, length-1, newhead)
+        else:
+            retArray = np.append(retArray, permutationGenerator(states, length-1, newhead), axis=0)
+
+    return retArray
 
 '''
 The Sim itself, streamlined function which takes in some inputs and synchronizes the planetary and solar sail animation
