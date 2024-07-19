latest changes. do not trust anything written. theoretically fixed sign issue in training loop

rl_agent/Agent.py
@@ -20,8 +20,8 @@ class Agent():
         self.learning_rate_Q = learning_rate_Q
         self.decay_rate = decay_rate
         
-        self.p_optimizer = tf.keras.optimizers.Adam()
-        self.q_optimizer = tf.keras.optimizers.Adam()
+        self.p_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate_policy)
+        self.q_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate_Q)
 
     def train(self, max_duration, episodes_per_epoch, epochs):
         pass
@@ -173,10 +173,10 @@ class ParallelAgent(Agent):
 
             # update policy parameters theta += alpha * Q(s,a) * grad log pi(a|s)
             with policy_tape:
-                policy_grad_target = self.learning_rate_policy * Q_SA * log_prob
+                policy_grad_target = -Q_SA * log_prob
             policy_grads = policy_tape.gradient(policy_grad_target, 
                                                 self.policy.trainable_variables)
-            print(f'pi nan printing... {sigma}') if any([np.isnan(a).any() for a in policy_grads]) else None
+
             if not any([tf.math.reduce_any(tf.math.is_nan(a)) for a in policy_grads]):
                 self.p_optimizer.apply_gradients(zip(policy_grads, 
                                                      self.policy.trainable_variables))
@@ -187,9 +187,8 @@ class ParallelAgent(Agent):
                 Q_SA_next = self.Q(SA_next)
             td_error = R + self.decay_rate * Q_SA_next - Q_SA
             with Q_tape:
-                Q_grad_target = self.learning_rate_Q * td_error.numpy() * Q_SA # TODO: confirm td error not differentiated
+                Q_grad_target = -td_error.numpy() * Q_SA # TODO: confirm td error not differentiated
             Q_grads = Q_tape.gradient(Q_SA, self.Q.trainable_variables)
-            [print(f'Q nan printing...') if np.isnan(a).any() else None for a in Q_grads]
 
             # update weights of Q: w += alpha * delta * grad Q(s,a)
             if not any([tf.math.reduce_any(tf.math.is_nan(a)) for a in Q_grads]):
@@ -284,8 +283,7 @@ class ParallelAgent(Agent):
                     mu, sigma = self.policy(s[:-1])
                     normal = tfp.distributions.Normal(mu, sigma)
                     log_prob = normal.log_prob(a[:-1])
-                    policy_grad_target = \
-                            self.learning_rate_policy * Qsa[:-1] * log_prob
+                    policy_grad_target = -Qsa[:-1] * log_prob
 
                 # update policy params
                 policy_grads = policy_tape.gradient(policy_grad_target, 
@@ -296,7 +294,7 @@ class ParallelAgent(Agent):
 
                 # update Q function
                 with Q_tape:
-                    Q_grad_target = self.learning_rate_Q * td_error * Qsa[:-1]
+                    Q_grad_target = -td_error * Qsa[:-1]
                 Q_grads = Q_tape.gradient(Q_grad_target, self.Q.trainable_variables)
                 self.q_optimizer.apply_gradients(zip(Q_grads, self.Q.trainable_variables))
         

rl_agent/World.py
@@ -69,6 +69,8 @@ class ParallelTrackNEO(ParallelWorld):
     def __init__(self, num_sails=50, dt=5, control_interval=5):
         self.time = {'get pos': 0, 'square dists': 0, 'grav accel': 0, 'sail accel': 0, 'update': 0}
 
+        self.orbit_dist_prev = None
+
         self.num_sails = num_sails
         self.dt_hours = dt
         self.dt = self.dt_hours * 3600
@@ -137,7 +139,7 @@ class ParallelTrackNEO(ParallelWorld):
 
 
     def reset(self, new_t0=None):
-        self.time = {}
+        self.time = {'get pos': 0, 'square dists': 0, 'grav accel': 0, 'sail accel': 0, 'update': 0}
 
         if new_t0 is not None:
             timeObj = spice.Time(*new_t0, 360) # jan 01, 2000, 360 days
@@ -241,17 +243,19 @@ class ParallelTrackNEO(ParallelWorld):
             self.t += 1
 
         # Reward
-        r_scalar = np.sum(r, axis=2)
+        r_scalar = np.sqrt(r2)
         reward_tdist = np.exp(-10/ParallelTrackNEO.AU * \
             r_scalar[:,self.target_body:self.target_body+1])
 
-        orbit_dist = r_scalar[:,0:1] - self.mean_target_r
+        orbit_dist = (r_scalar[:,0:1] - self.mean_target_r) / ParallelTrackNEO.AU
 
         if self.orbit_dist_prev is not None:
             reward_orbit_error = orbit_dist - self.orbit_dist_prev
         else:
-            reward_orbit_error = 0
-        reward_orbit_error = np.clip(reward_orbit_error, -1, 1)
+            reward_orbit_error = np.zeros(orbit_dist.shape)
+        reward_orbit_error[reward_orbit_error > 0] *= 10
+        reward_orbit_error[reward_orbit_error < 0] *= 5
+        reward_orbit_error = np.tanh(reward_orbit_error)
 
         self.orbit_dist_prev = orbit_dist
 